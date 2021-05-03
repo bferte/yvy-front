6554efcb3a939efd16c8730c703921710d24b2c6
Briac ferté <bferte@mbp-de-briac.home>
2021-05-03 16:56:31 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
-import '../styles/globals.css'
+//import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/client'
+import { Provider } from 'next-auth/client'
+
+import Layout from '../components/common/Layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider session={pageProps.session}>
+        {Component.auth
+          ? <Auth><Layout><Component {...pageProps} /></Layout></Auth>
+          : <Layout><Component {...pageProps} /></Layout>
+        }
+    </Provider>
+  )
 }
 
+function Auth({ children }) {
+  const [session, loading] = useSession()
+  const isUser = !!session?.user
+  const router = useRouter()
+  useEffect(() => {
+    if (loading) return // Do nothing while loading
+    if (!isUser) router.push('/login')
+    // If not authenticated, force log in
+  }, [isUser, loading])
+  if (isUser) {
+    return children
+  }
+  // Session is being fetched, or no user.
+  // If no user, useEffect() will redirect.
+  return <div>Loading...</div>
+}
 export default MyApp
